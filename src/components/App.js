814284da48dc2fd76f50a88df6c3d67a63d10161
Harsh652003 <156173288+Harsh652003@users.noreply.github.com>
2025-01-04 14:17:31 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import LoginForm from "./LoginForm";
+import "./../styles/App.css";
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>{isLoggedIn ? "Welcome, User!" : "Please Log In"}</h1>
+      {!isLoggedIn && <LoginForm onLogin={handleLogin} />}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
